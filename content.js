cf7aa653655eafb1cb44b84b063a450d5263e804
Trophy198 <spsp8426@naver.com>
2024-04-13 15:35:34 +0900

    add description when try login

--- content.js
+++ content.js
@@ -11,16 +11,19 @@ const container = document.querySelector('.overlay-container');
 const login_tag = document.createElement('p');
 const image = document.querySelector(".overlay-container img");
 
-login_tag.setAttribute('class','overlay-footer');
-login_tag.innerHTML = rustplusplus_login_description;
-login_tag.style.color = '#A4A6A7';
-login_tag.style.paddingTop = '10px';
-container.appendChild(login_tag);
 
 if (button && description && image) {
     // button text
     // button.textContent = "Login";
 
+
+
     // description
+    login_tag.setAttribute('class','overlay-footer');
+    login_tag.innerHTML = rustplusplus_login_description;
+    login_tag.style.color = '#A4A6A7';
+    login_tag.style.paddingTop = '10px';
+    container.appendChild(login_tag);
+    
     description.textContent = "The Credentials are required to pair rustplusplus with different Rust servers and Smart Devices. It also allow for different notifications that enhances the gaming experience such as Offline Death notifications, teammate login notifications and more.";
 };
