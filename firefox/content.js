b3a0bf0bf6af1c9ebb413ec363dc83c0d18c9913
Trophy198 <spsp8426@naver.com>
2024-04-15 20:36:39 +0900

    refactor content script for early DOM manipulation

--- firefox/content.js
+++ firefox/content.js
@@ -1,48 +1,43 @@
-const ReactNativeWebView = {
-    postMessage: function(message) {
-        try {
-            var auth = JSON.parse(message);
-            if (auth && auth.Token && auth.SteamId) {
-                const baseUrl = 'https://rustplusplus-credentials.netlify.app/callback';
-                const queryParams = `?token=${encodeURIComponent(auth.Token)}&steamId=${encodeURIComponent(auth.SteamId)}`;
-                window.location.href = baseUrl + queryParams;
-            } else {
-                console.error('Invalid authentication message received:', message);
+if (typeof window.wrappedJSObject.ReactNativeWebView === 'undefined') {
+    const ReactNativeWebView = {
+        postMessage: function(message) {
+            try {
+                const auth = JSON.parse(message);
+                if (auth && auth.Token && auth.SteamId) {
+                    const baseUrl = 'https://rustplusplus-credentials.netlify.app/callback';
+                    const queryParams = `?token=${encodeURIComponent(auth.Token)}&steamId=${encodeURIComponent(auth.SteamId)}`;
+                    window.location.href = baseUrl + queryParams;
+                }
+            } catch (e) {
+                console.error('Error parsing message:', message, e);
             }
-        } catch (e) {
-            console.error('Error parsing message:', message, e);
         }
-    }
-};
+    };
 
-if (typeof cloneInto !== 'undefined') {
     window.wrappedJSObject.ReactNativeWebView = cloneInto(
         ReactNativeWebView,
         window, {
             cloneFunctions: true
         }
     );
-} else {
-    window.ReactNativeWebView = ReactNativeWebView;
 }
 
-const rustplusplus_login_description = 'Login to retrieve your credentials for rustplusplus.';
-const button = document.querySelector("form button span");
-const description = document.querySelector(".overlay-body p");
-const container = document.querySelector('.overlay-container');
-const login_tag = document.createElement('p');
-const image = document.querySelector(".overlay-container img");
+function updateDOM() {
+    const description = document.querySelector(".overlay-body p");
+    const container = document.querySelector('.overlay-container');
 
-if (button && description && image) {
-    // Update button text
-    // button.textContent = "Login";
-
-    // Set attributes for login_tag
-    login_tag.setAttribute('class', 'overlay-footer');
-    login_tag.textContent = rustplusplus_login_description; 
-    login_tag.style.color = '#A4A6A7';
-    login_tag.style.paddingTop = '10px';
-    container.appendChild(login_tag);
-    
-    description.textContent = "The Credentials are required to pair rustplusplus with different Rust servers and Smart Devices. It also allow for different notifications that enhances the gaming experience such as Offline Death notifications, teammate login notifications and more.";
+    if (description && container) {
+        const login_tag = document.createElement('p');
+        login_tag.setAttribute('class', 'overlay-footer');
+        login_tag.textContent = 'Login to retrieve your credentials for rustplusplus.';
+        login_tag.style.color = '#A4A6A7';
+        login_tag.style.paddingTop = '10px';
+        container.appendChild(login_tag);
+        
+        description.textContent = "The Credentials are required to pair rustplusplus with different Rust servers and Smart Devices. It also allows for different notifications that enhance the gaming experience such as Offline Death notifications, teammate login notifications and more.";
+    } else {
+        requestAnimationFrame(updateDOM);
+    }
 }
+
+requestAnimationFrame(updateDOM);
