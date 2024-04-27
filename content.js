5c09b8fa372995e5d83de481b82405ba2f662fe7
Trophy198 <spsp8426@naver.com>
2024-04-27 23:46:17 +0900

    refactor : added chrome extension detection feature

--- content.js
+++ content.js
@@ -1,29 +1,35 @@
-const inject = document.createElement('script');
-inject.src = chrome.runtime.getURL('catcher.js');
-inject.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(inject);
-const rustplusplus_login_description = 'Login to retrieve your credentials for rustplusplus.'
-const button = document.querySelector("form button span");
-const description = document.querySelector(".overlay-body p");
-const container = document.querySelector('.overlay-container');
-const login_tag = document.createElement('p');
-const image = document.querySelector(".overlay-container img");
+document.addEventListener('DOMContentLoaded', function () {
+    if (location.href.includes('https://rustplusplus-credentials.netlify.app')) {
+        const manifest = chrome.runtime.getManifest();
+        let beacon = document.createElement("div");
+        beacon.innerText = manifest.version;
+        beacon.style.display = 'none';
+        beacon.id = "chromeAddon";
+        document.body.appendChild(beacon);
+    } else {
+        const inject = document.createElement('script');
+        inject.src = chrome.runtime.getURL('catcher.js');
+        inject.onload = function() {
+            this.remove();
+        };
+        (document.head || document.documentElement).appendChild(inject);
 
+        const rustplusplus_login_description = 'Login to retrieve your credentials for rustplusplus.';
+        const button = document.querySelector("form button span");
+        const description = document.querySelector(".overlay-body p");
+        const container = document.querySelector('.overlay-container');
+        const login_tag = document.createElement('p');
+        const image = document.querySelector(".overlay-container img");
 
-if (button && description && image) {
-    // button text
-    // button.textContent = "Login";
-
-
-
-    // description
-    login_tag.setAttribute('class','overlay-footer');
-    login_tag.innerHTML = rustplusplus_login_description;
-    login_tag.style.color = '#A4A6A7';
-    login_tag.style.paddingTop = '10px';
-    container.appendChild(login_tag);
-    
-    description.textContent = "The Credentials are required to pair rustplusplus with different Rust servers and Smart Devices. It also allow for different notifications that enhances the gaming experience such as Offline Death notifications, teammate login notifications and more.";
-};
+        if (button && description && image) {
+            // description
+            login_tag.setAttribute('class', 'overlay-footer');
+            login_tag.innerHTML = rustplusplus_login_description;
+            login_tag.style.color = '#A4A6A7';
+            login_tag.style.paddingTop = '10px';
+            container.appendChild(login_tag);
+            
+            description.textContent = "The Credentials are required to pair rustplusplus with different Rust servers and Smart Devices. It also allows for different notifications that enhance the gaming experience such as Offline Death notifications, teammate login notifications and more.";
+        };
+    }
+});
